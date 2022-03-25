2f5e043a7b5b0eb9b330acb29b4f12fc8f8c615a
Marcus Posey <marcusposey@gmail.com>
2022-03-25 16:40:29 -0400

    Stub Collection service

--- server/collection.go
+++ server/collection.go
@@ -1,7 +1,20 @@
 package server
 
-import "z4/proto"
+import (
+	"context"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+	"z4/proto"
+)
 
 type collection struct {
 	proto.UnimplementedCollectionServer
+}
+
+func (c *collection) CreateTask(ctx context.Context, req *proto.CreateTaskRequest) (*proto.Task, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method CreateTask not implemented")
+}
+
+func (c *collection) StreamTasks(req *proto.StreamTasksRequest, stream proto.Collection_StreamTasksServer) error {
+	return status.Errorf(codes.Unimplemented, "method StreamTasks not implemented")
 }
