e22df8babd5b062dc6763980cb2bd107b3fbc8ec
Marcus Posey <marcusposey@gmail.com>
2022-03-25 17:04:44 -0400

    Implement CreateTask rpc using simple store

--- server/collection.go
+++ server/collection.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
+	"google.golang.org/protobuf/types/known/timestamppb"
 	"z4/proto"
 	"z4/storage"
 )
@@ -18,7 +19,21 @@ func newCollection() proto.CollectionServer {
 }
 
 func (c *collection) CreateTask(ctx context.Context, req *proto.CreateTaskRequest) (*proto.Task, error) {
-	return nil, status.Errorf(codes.Unimplemented, "method CreateTask not implemented")
+	t, err := c.store.Save(ctx, storage.TaskDefinition{
+		RunTime:  req.GetDeliverAt().AsTime(),
+		Metadata: req.GetMetadata(),
+		Payload:  req.GetPayload(),
+	})
+
+	if err != nil {
+		return nil, status.Errorf(codes.Internal, "failed to save task: %v", err)
+	}
+	return &proto.Task{
+		Metadata:  t.Metadata,
+		Payload:   t.Payload,
+		DeliverAt: timestamppb.New(t.RunTime),
+		Id:        t.ID,
+	}, nil
 }
 
 func (c *collection) StreamTasks(req *proto.StreamTasksRequest, stream proto.Collection_StreamTasksServer) error {
