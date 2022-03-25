e771b63836ffb91dfae1f82efe76a953395f916c
Marcus Posey <marcusposey@gmail.com>
2022-03-25 16:37:28 -0400

    Stub health check

--- server/admin.go
+++ server/admin.go
@@ -1,7 +1,15 @@
 package server
 
-import "z4/proto"
+import (
+	"context"
+	"z4/proto"
+)
 
 type admin struct {
 	proto.UnimplementedAdminServer
+}
+
+func (a *admin) CheckHealth(ctx context.Context, req *proto.CheckHealthRequest) (*proto.Status, error) {
+	// TODO: Implement real health check.
+	return new(proto.Status), nil
 }
