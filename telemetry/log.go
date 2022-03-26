4d458acc58c867a81d4fb1f5aadb649f66edf8a6
Marcus Posey <marcusposey@gmail.com>
2022-03-26 12:18:04 -0400

    Document logger

--- telemetry/log.go
+++ telemetry/log.go
@@ -4,8 +4,16 @@ import (
 	"go.uber.org/zap"
 )
 
+// Logger is the global logger.
+//
+// Clients should use this directly after it has
+// been configured with the InitLogger method.
 var Logger *zap.Logger
 
+// InitLogger initializes Logger.
+//
+// Enabling debug logging will display logs in a more readable
+// format in addition to displaying debug-level logs and above.
 func InitLogger(debugEnabled bool) error {
 	var err error
 	if debugEnabled {
@@ -16,6 +24,7 @@ func InitLogger(debugEnabled bool) error {
 	return err
 }
 
+// LogRequestID returns a zap log field for http request IDs.
 func LogRequestID(id string) zap.Field {
 	return zap.String("request_id", id)
 }
