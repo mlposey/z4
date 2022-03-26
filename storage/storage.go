7175710ce25ab9e681920fa72e9b1c5462c27cb6
Marcus Posey <marcusposey@gmail.com>
2022-03-26 12:23:49 -0400

    Make simple store thread safe

--- storage/storage.go
+++ storage/storage.go
@@ -3,6 +3,7 @@ package storage
 import (
 	"context"
 	"math/rand"
+	"sync"
 	"time"
 )
 
@@ -27,8 +28,10 @@ type TaskRange struct {
 //
 // This is a very inefficient, in-memory implementation of a task store.
 // It should not be used in production.
+// It is safe for concurrent use by multiple goroutines.
 type SimpleStore struct {
 	tasks []Task
+	mu    sync.RWMutex
 }
 
 func (s *SimpleStore) Save(ctx context.Context, task TaskDefinition) (Task, error) {
@@ -36,11 +39,16 @@ func (s *SimpleStore) Save(ctx context.Context, task TaskDefinition) (Task, erro
 		ID:             rand.Int63(),
 		TaskDefinition: task,
 	}
+	s.mu.Lock()
 	s.tasks = append(s.tasks, t)
+	s.mu.Unlock()
 	return t, nil
 }
 
 func (s *SimpleStore) Get(ctx context.Context, tr TaskRange) ([]Task, error) {
+	s.mu.RLock()
+	defer s.mu.RUnlock()
+
 	var tasks []Task
 	for _, task := range s.tasks {
 		if task.RunTime.After(tr.Min) && task.RunTime.Before(tr.Max) {
@@ -51,6 +59,9 @@ func (s *SimpleStore) Get(ctx context.Context, tr TaskRange) ([]Task, error) {
 }
 
 func (s *SimpleStore) Delete(ctx context.Context, task Task) error {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+
 	idx := -1
 	for i, t := range s.tasks {
 		if t.ID == task.ID {
