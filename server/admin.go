a37f60d5daec3fe08109afc846f5180ce7893baa
Marcus Posey <marcusposey@gmail.com>
2022-04-02 21:42:10 -0400

    Expand admin api functionality

--- server/admin.go
+++ server/admin.go
@@ -4,6 +4,8 @@ import (
 	"context"
 	"github.com/hashicorp/raft"
 	"github.com/mlposey/z4/proto"
+	"github.com/mlposey/z4/telemetry"
+	"go.uber.org/zap"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 	emptypb "google.golang.org/protobuf/types/known/emptypb"
@@ -35,10 +37,23 @@ func (a *admin) GetClusterInfo(
 	ctx context.Context,
 	req *proto.GetClusterInfoRequest,
 ) (*proto.ClusterInfo, error) {
+	var members []*proto.Server
+	config := a.raft.GetConfiguration()
+	if err := config.Error(); err != nil {
+		telemetry.Logger.Error("failed to fetch server list", zap.Error(err))
+	} else {
+		for _, server := range config.Configuration().Servers {
+			members = append(members, &proto.Server{
+				Id:      string(server.ID),
+				Address: string(server.Address),
+			})
+		}
+	}
 
 	return &proto.ClusterInfo{
 		ServerId:      a.serverID,
 		LeaderAddress: string(a.raft.Leader()),
+		Members:       members,
 	}, nil
 }
 
@@ -53,6 +68,21 @@ func (a *admin) AddClusterMember(
 	id := raft.ServerID(req.GetMemberId())
 	addr := raft.ServerAddress(req.GetMemberAddress())
 	future := a.raft.AddVoter(id, addr, 0, 0)
+	err := future.Error()
+	return new(emptypb.Empty), status.Errorf(codes.Internal,
+		"could not add member to cluster: %v", err)
+}
+
+func (a *admin) RemoveClusterMember(
+	ctx context.Context,
+	req *proto.RemoveClusterMemberRequest,
+) (*emptypb.Empty, error) {
+	// TODO: For now, assume this is always sent to the right node: the leader.
+	// If we can detect within the server if we are the leader, we can forward
+	// the request. Not sure how to do that right now
+
+	id := raft.ServerID(req.GetMemberId())
+	future := a.raft.RemoveServer(id, 0, 0)
 	err := future.Error()
 	return new(emptypb.Empty), status.Errorf(codes.Internal,
 		"could not add member to cluster: %v", err)
