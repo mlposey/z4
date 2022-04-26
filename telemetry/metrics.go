83ed2e0653ab4745f8513b1c3eed4d32f715073e
Marcus Posey <marcusposey@gmail.com>
2022-04-26 19:29:13 -0400

    Use prefix for prometheus metrics

--- telemetry/metrics.go
+++ telemetry/metrics.go
@@ -8,30 +8,38 @@ import (
 	"net/http"
 )
 
+const prometheusNamespace = "z4"
+
 var (
 	ReceivedLogs = promauto.NewCounter(prometheus.CounterOpts{
-		Name: "received_log_count",
-		Help: "The total number of Raft logs sent for application to the fsm",
+		Namespace: prometheusNamespace,
+		Name:      "received_log_count",
+		Help:      "The total number of Raft logs sent for application to the fsm",
 	})
 	AppliedLogs = promauto.NewCounter(prometheus.CounterOpts{
-		Name: "applied_log_count",
-		Help: "The total number of Raft logs successfully applied/saved to the fsm",
+		Namespace: prometheusNamespace,
+		Name:      "applied_log_count",
+		Help:      "The total number of Raft logs successfully applied/saved to the fsm",
 	})
 	LastFSMSnapshot = promauto.NewGauge(prometheus.GaugeOpts{
-		Name: "last_fsm_snapshot",
-		Help: "The unix time in seconds when the last fsm snapshot was taken",
+		Namespace: prometheusNamespace,
+		Name:      "last_fsm_snapshot",
+		Help:      "The unix time in seconds when the last fsm snapshot was taken",
 	})
 	LastDBGC = promauto.NewGauge(prometheus.GaugeOpts{
-		Name: "last_db_gc",
-		Help: "The unix tine in seconds when the last gc phase was run for the database",
+		Namespace: prometheusNamespace,
+		Name:      "last_db_gc",
+		Help:      "The unix tine in seconds when the last gc phase was run for the database",
 	})
 	StreamedTasks = promauto.NewCounterVec(prometheus.CounterOpts{
-		Name: "streamed_task_total",
-		Help: "The total number of tasks sent to clients",
+		Namespace: prometheusNamespace,
+		Name:      "streamed_task_total",
+		Help:      "The total number of tasks sent to clients",
 	}, []string{"method", "namespace"})
 	CreateTaskRequests = promauto.NewCounterVec(prometheus.CounterOpts{
-		Name: "create_task_request_total",
-		Help: "The total number of requests from clients to create a task",
+		Namespace: prometheusNamespace,
+		Name:      "create_task_request_total",
+		Help:      "The total number of requests from clients to create a task",
 	}, []string{"method", "namespace"})
 )
 
