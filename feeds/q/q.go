f568a5b4bd6c215006ef361157c35466548eb507
Marcus Posey <marcusposey@gmail.com>
2022-05-15 17:19:00 -0400

    Document q types

--- feeds/q/q.go
+++ feeds/q/q.go
@@ -6,33 +6,63 @@ import (
 	"time"
 )
 
+// A TaskWriter writes task-related entities to a collection.
 type TaskWriter interface {
+	// Push should add tasks to the collection.
+	//
+	// isFollower indicates whether this operation is
+	// being performed on a cluster follower (as opposed
+	// to the leader).
 	Push(tasks []*proto.Task, isFollower bool) error
+
+	// Acknowledge should apply task acknowledgements
+	// to the collection.
 	Acknowledge(acks []*proto.Ack) error
+
+	// PurgeTasks should delete all tasks in the collection.
 	PurgeTasks(namespace string) error
+
+	// Close should release all resources needed to
+	// safely destroy the TaskWriter object.
 	Close() error
 }
 
+// A TaskStream is an unbounded collection of tasks.
 type TaskStream <-chan *proto.Task
 
+// A Reader reads tasks from a collection.
 type Reader interface {
+	// Ready signals whether Read *should* be invoked.
+	//
+	// This can be used to prevent concurrent Reads
+	// if the Read operation will take a long time.
 	Ready() bool
+
+	// Read should call f on each task in the collection.
 	Read(f func(task *proto.Task) error) error
 }
 
+// A QueryFactory generates task queries.
 type QueryFactory interface {
 	Query(namespace *proto.Namespace) storage.TaskRange
 }
 
+// A PreemptiveQueryFactory generates task queries.
+//
+// It is much like a QueryFactory but contains an
+// additional method Inform that can help the factory
+// build queries based on the results of previous queries.
 type PreemptiveQueryFactory interface {
 	QueryFactory
 	Inform(n int)
 }
 
+// A Checkpointer saves a checkpoint based on a task.
 type Checkpointer interface {
 	Set(namespace *proto.Namespace, task *proto.Task)
 }
 
+// Readers instantiates a list of all possible readers.
 func Readers(
 	tasks *storage.TaskStore,
 	namespace *proto.Namespace,
