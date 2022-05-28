1306a3b6e3c4c886b90642519890c10d39d0f19d
Marcus Posey <marcusposey@gmail.com>
2022-05-28 11:42:49 -0400

    Update tests to use client

--- tests/task_streams_test.go
+++ tests/task_streams_test.go
@@ -6,10 +6,14 @@ import (
 	"fmt"
 	"github.com/cucumber/godog"
 	"github.com/google/uuid"
+	"github.com/mlposey/z4/pkg/z4"
 	"github.com/mlposey/z4/proto"
 	"github.com/mlposey/z4/tests/util"
 	"go.uber.org/multierr"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
 	pb "google.golang.org/protobuf/proto"
+	"sync"
 	"testing"
 	"time"
 )
@@ -37,10 +41,13 @@ func TestTaskStreaming(t *testing.T) {
 type taskStreams struct {
 	server        *util.LocalServer
 	serverPort    int
-	client        *util.Client
+	producer      *z4.UnaryProducer
+	consumer      *z4.Consumer
 	taskRequest   *proto.PushTaskRequest
 	createdTask   *proto.Task
 	receivedTasks []*proto.Task
+	conn          *grpc.ClientConn
+	taskMu        sync.Mutex
 }
 
 func (ts *taskStreams) setupSuite() error {
@@ -50,28 +57,39 @@ func (ts *taskStreams) setupSuite() error {
 	ts.createdTask = nil
 	ts.receivedTasks = nil
 
-	if err := ts.server.Start(); err != nil {
+	err := ts.server.Start()
+	if err != nil {
 		return err
 	}
 
-	c, err := util.NewClient("localhost", ts.serverPort)
-	if err == nil {
-		ts.client = c
+	opts := []grpc.DialOption{grpc.WithTransportCredentials(insecure.NewCredentials())}
+	target := fmt.Sprintf("localhost:%d", ts.serverPort)
+	ts.conn, err = grpc.Dial(target, opts...)
+	if err != nil {
+		return err
 	}
+
+	ts.producer, err = z4.NewUnaryProducer(z4.UnaryProducerOptions{
+		ProducerOptions: z4.ProducerOptions{Conn: ts.conn},
+	})
 	return err
 }
 
 func (ts *taskStreams) teardownSuite() error {
-	err1 := ts.client.Close()
+	err1 := ts.conn.Close()
 	err2 := ts.server.Stop()
 	return multierr.Combine(err1, err2)
 }
 
 func (ts *taskStreams) afterSecondsIShouldReceiveTheSameTask(arg1 int) error {
 	time.Sleep(time.Duration(arg1) * time.Second)
+
+	ts.taskMu.Lock()
+	defer ts.taskMu.Unlock()
 	if len(ts.receivedTasks) != 1 {
 		return fmt.Errorf("expected to receive the input task back; got %d tasks", len(ts.receivedTasks))
 	}
+
 	ok := pb.Equal(ts.receivedTasks[0], ts.createdTask)
 	if !ok {
 		return fmt.Errorf("tasks not equal; expected %v; got %v", ts.createdTask, ts.receivedTasks[0])
@@ -81,6 +99,9 @@ func (ts *taskStreams) afterSecondsIShouldReceiveTheSameTask(arg1 int) error {
 
 func (ts *taskStreams) afterSecondsIShouldReceiveTasks(arg1, arg2 int) error {
 	time.Sleep(time.Duration(arg1) * time.Second)
+
+	ts.taskMu.Lock()
+	defer ts.taskMu.Unlock()
 	if len(ts.receivedTasks) != arg2 {
 		return fmt.Errorf("expected %d tasks; got %d", arg2, len(ts.receivedTasks))
 	}
@@ -93,25 +114,25 @@ func (ts *taskStreams) iBeginStreamingAfterASecondDelay(arg1 int) error {
 }
 
 func (ts *taskStreams) consumeTaskStream(namespace string) error {
-	stream, err := ts.client.PullTasks(uuid.New().String(), namespace)
-	if err != nil {
-		return err
-	}
-	responses, err := stream.Listen()
+	var err error
+	ts.consumer, err = z4.NewConsumer(z4.ConsumerOptions{
+		Conn:      ts.conn,
+		Namespace: namespace,
+	})
 	if err != nil {
 		return err
 	}
 
 	go func() {
-		for res := range responses {
-			if res.Error != nil {
-				fmt.Println(res.Error)
-				break
-			}
+		_ = ts.consumer.Consume(func(m z4.Message) error {
 
-			ts.receivedTasks = append(ts.receivedTasks, res.Task)
-			res.Ack()
-		}
+			ts.taskMu.Lock()
+			ts.receivedTasks = append(ts.receivedTasks, m.Task())
+			ts.taskMu.Unlock()
+
+			m.Ack()
+			return nil
+		})
 	}()
 	return nil
 }
@@ -130,8 +151,8 @@ func (ts *taskStreams) iHaveCreatedTheTask(arg1 *godog.DocString) error {
 			TtsSeconds: int64(taskDef["tts_seconds"].(float64)),
 		},
 	}
-	task, err := ts.client.Push(ts.taskRequest)
-	ts.createdTask = task.GetTask()
+	task, err := ts.producer.CreateTask(context.Background(), ts.taskRequest)
+	ts.createdTask = task
 	return err
 }
 
